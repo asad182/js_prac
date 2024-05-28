ef8258b14c14c5cd0e40b898923159e13777dfdf
asad182 <asadmehmood@chainfore.com>
2024-05-28 12:24:29 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,33 @@
+//let score = "33abc"
+//let score = null
+//let score = undefined
+//let score = false
+// let score = "abc"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+/*
+33 -> 33
+33abc -> NaN
+true -> 1 , false -> 0
+*/
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+1 -> True
+0 -> False
+"" -> False
+"abc" -> True
+*/
+
+let someNumber = 12
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
